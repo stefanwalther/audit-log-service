961b9ddb6a6530a5f306f60a7b11ac174672125d
Stefan Walther <Stefan.Walther@qlik.com>
2016-12-22 23:00:52 +0100

    test: First test flow

--- test/integration/logs.spec.js
+++ test/integration/logs.spec.js
@@ -1,6 +1,24 @@
-describe('jobs => integration', () => {
+const superTest = require('supertest-as-promised');
+const HttpStatus = require('http-status-codes');
+const AppServer = require('./../../src/app-server');
 
-  it('should ideally succeed', () => {
+const defaultConfig = require('./../test-lib/default-config');
+
+describe('jobs => integration', () => {
+  let server;
+  const appServer = new AppServer(defaultConfig);
+  before(() => {
+    return appServer.start()
+      .then(() => {
+        server = superTest(appServer.server);
+      });
+  });
+
+  after(() => {
+    return appServer.stop();
+  });
+
+  it('', () => {
     expect(true).to.be.true;
   });
 });
