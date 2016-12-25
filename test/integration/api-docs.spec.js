324510b6f59115ee88e9e88a76ae232bdae826b2
Stefan Walther <Stefan.Walther@qlik.com>
2016-12-26 00:28:58 +0100

    test: integration tests for /api-docs/

--- test/integration/api-docs.spec.js
+++ test/integration/api-docs.spec.js
@@ -1,5 +1,32 @@
-describe('api-docs', () => {
-  it('are available', () => {
-    expect(true).to.be.true;
+const superTest = require('supertest-as-promised');
+const HttpStatus = require('http-status-codes');
+const AppServer = require('./../../src/app-server');
+
+const defaultConfig = require('./../test-lib/default-config');
+
+describe('jobs => integration', () => {
+  let server;
+  const appServer = new AppServer(defaultConfig);
+  before(() => {
+    return appServer.start()
+      .then(() => {
+        server = superTest(appServer.server);
+      });
+  });
+
+  after(() => {
+    return appServer.stop();
+  });
+
+  it('GET `/api-docs` => returns redirection to /api-docs/', () => {
+    return server
+      .get('/api-docs')
+      .expect(HttpStatus.MOVED_PERMANENTLY);
+  });
+
+  it('GET `/api-docs/` => returns the api-docs', () => {
+    return server
+      .get('/api-docs/')
+      .expect(HttpStatus.OK);
   });
 });
