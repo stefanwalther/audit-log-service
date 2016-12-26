c1afc648f777d218ab9c0952dd9274b9d4919103
Stefan Walther <Stefan.Walther@qlik.com>
2016-12-26 22:53:27 +0100

    feat: Make api-docs version dynamic

--- src/route-config.js
+++ src/route-config.js
@@ -3,6 +3,7 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 const path = require('path');
 const swaggerUi = require('swagger-ui-express');
+const pkg = require('./../package.json');
 
 const LogsController = require('./modules/logs/logs.controller');
 
@@ -19,7 +20,8 @@ function init(app) {
 
   // Swagger
   const swaggerDoc = yaml.safeLoad(fs.readFileSync(path.join(__dirname, './config/api-docs.yml'), 'utf8'));
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+  swaggerDoc.info.version = pkg.version;
+  app.use('/api-docs/', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
   app.use('/', router);
 }
