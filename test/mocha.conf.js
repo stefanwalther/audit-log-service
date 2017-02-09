793622610e71905a9db8c0350cfacac0558336cb
Stefan Walther <Stefan.Walther@qlik.com>
2017-02-10 00:28:44 +0100

    ci: Set SAMMLER_DB_URI_LOGS not in CircleCI environment

--- test/mocha.conf.js
+++ test/mocha.conf.js
@@ -1,4 +1,6 @@
 process.env.NODE_ENV = 'test';
-process.env.SAMMLER_DB_URI_LOGS = 'mongodb://localhost:27018/logs';
+if (process.env.CIRCLECI !== 'true') {
+  process.env.SAMMLER_DB_URI_LOGS = 'mongodb://localhost:27018/logs';
+}
 global.expect = require('chai').expect;
 
