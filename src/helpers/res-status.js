f86162d918e1f8caa573e906da15b845cc63711c
Stefan Walther <Stefan.Walther@qlik.com>
2017-02-13 21:56:29 +0100

    refactor: Simplify res-status.js

--- src/helpers/res-status.js
+++ src/helpers/res-status.js
@@ -3,18 +3,15 @@ const HttpStatus = require('http-status-codes');
 class ResStatus {
 
   static ok(res, result) {
-    res.status(HttpStatus.OK);
-    res.json(result);
+    res.status(HttpStatus.OK).json(result);
   }
 
   static created(res, result) {
-    res.status(HttpStatus.CREATED);
-    res.json(result);
+    res.status(HttpStatus.CREATED).json(result);
   }
 
   static error(res, err) {
-    res.status(HttpStatus.INTERNAL_SERVER_ERROR);
-    res.json(err);
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
   }
 
 }
